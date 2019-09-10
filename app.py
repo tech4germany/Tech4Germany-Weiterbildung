9da509829f12943a35be49c4ed666a8e44a4ecb6
Manuel Lang <manuellang183@gmail.com>
2019-09-10 18:14:31 +0200

    more examples

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 # example showing how to use Flask
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# index route handler
+# @app.route("/")
+# def hello():
+#     return "Hello Tech4Germany"
+
+# index route handler with template
 @app.route("/")
 def hello():
-    return "Hello Tech4Germany!"
+    return render_template('example.html')
+
+# custom route handler
+@app.route("/<text>")
+def custom_hello(text):
+    return render_template('example2.html', text=text)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
