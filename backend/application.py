9ad20919d0196ea1c249d5f6f4a66b49e197aa8b
Manuel Lang <manuellang183@gmail.com>
2019-09-19 18:38:42 +0200

    implemented regex for title matching

--- backend/application.py
+++ backend/application.py
@@ -5,6 +5,7 @@ from bson import json_util
 import json
 import os
 from pymongo import MongoClient
+import re
 
 application = Flask(__name__)
 CORS(application)
@@ -14,25 +15,47 @@ cache.init_app(application)
 """
 Lists all existing courses
 """
-@application.route("/courses")
+@application.route("/courses/all")
 @cache.cached(timeout=50)
-def list_courses():
+def list_all_courses():
     connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     courses_collection = list(t4g_database.courses.find({}))
     return json.dumps(courses_collection, default=json_util.default)
 
+"""
+Lists all existing courses
+"""
+@application.route("/courses/all/<int:limit>")
+@cache.cached(timeout=50)
+def list_courses(limit):
+    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    mongo_client = MongoClient(connection_string)
+    t4g_database = mongo_client.t4g
+    courses_collection = list(t4g_database.courses.find({}).limit(limit))
+    return json.dumps(courses_collection, default=json_util.default)
 
 """
 Lists all existing courses with a given title
 """
-@application.route("/courses/<title>")
-def find_courses(title):
+@application.route("/courses/filter/<title>")
+def find_all_courses(title):
     connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
-    courses_collection = list(t4g_database.courses.find({ "title": title }))
+    courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': f'.*{title}.*' }}))
+    return json.dumps(courses_collection, default=json_util.default)
+
+"""
+Lists all existing courses with a given title and a specifc limit
+"""
+@application.route("/courses/filter/<title>/<int:limit>")
+def find_courses(title, limit):
+    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    mongo_client = MongoClient(connection_string)
+    t4g_database = mongo_client.t4g
+    courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': f'.*{title}.*' }}).limit(limit))
     return json.dumps(courses_collection, default=json_util.default)
 
 
