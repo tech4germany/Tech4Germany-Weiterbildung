2665003d5b24e1b04408e318a8310b8ca5e2a77e
Manuel Lang <manuellang183@gmail.com>
2019-09-20 13:03:05 +0200

    scraper output to s3

--- scraping/scrappy_scraper.py
+++ scraping/scrappy_scraper.py
@@ -3,14 +3,26 @@ from bs4 import BeautifulSoup
 import lxml
 import re
 from tqdm import tqdm as tqdm
+import boto3
 
 # for pickle
 import sys
 sys.setrecursionlimit(50000)
 
 last_layers = []
+BUCKET_NAME = 't4g-2019-bmas-kursnet-data'
+
+s3 = boto3.resource('s3')
 
 def crawl(targets, parent=None):
+    """Crawls and exports all courses under the given target from KursNet
+    
+    Arguments:
+        targets {list<String>} -- KursNet sections
+    
+    Keyword Arguments:
+        parent {[type]} -- [description] (default: {None})
+    """
     url = 'https://kursnet-finden.arbeitsagentur.de/kurs/systematiksuche.do?sss='
     
     for item in targets:
@@ -27,32 +39,35 @@ def crawl(targets, parent=None):
 
 
 def export_courses(layer):
+    """Exports all courses under the given layer
+    
+    Arguments:
+        layer {[type]} -- [description]
+    """
     url = f'https://kursnet-finden.arbeitsagentur.de/kurs/kursDetail.do?seite=1&sn={layer[1]}&doNext=detail&anzahlSeite=5000000'
-    try:
-        req = requests.get(url)
-        soup = BeautifulSoup(req.content, 'lxml')
-        for row in soup.find_all('a', title='Veranstaltungsdetail'):
-            export_course([layer[0], layer[1], row['href'].split('vg_id=')[1].split('&anzahl')[0]])
-    except:
-        print(f'error at layer {layer}')
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'lxml')
+    for row in soup.find_all('a', title='Veranstaltungsdetail'):
+        export_course([layer[0], layer[1], row['href'].split('vg_id=')[1].split('&anzahl')[0]])
 
 
 def export_course(course):
+    """Exports the given course
+    
+    Arguments:
+        course {[type]} -- [description]
+    """
     url = 'https://kursnet-finden.arbeitsagentur.de/kurs/veranstaltungsDetail.do?seite=1&anzahlSeite=5000000&doNext=vgdetail&vg_id='
         
-    try:
-        req = requests.get(url + course[-1])
-        soup = BeautifulSoup(req.content, 'lxml')
-        content = soup.find('div', id='inhalt')
-        title = content.find('h2', id='Titel').text
-        course_id = course[-1]
-        parent_1 = course[-2]
-        parent_2 = course[-3] if len(course) > 2 else ""
-        with open(f'./output/{course_id}_data.csv', 'w+') as outf:
-            outf.write(f'{title}, {course_id}, {parent_1}, {parent_2}, {content}\n')
+    req = requests.get(url + course[-1])
+    soup = BeautifulSoup(req.content, 'lxml')
+    content = soup.find('div', id='inhalt')
+    title = content.find('h2', id='Titel').text
+    course_id = course[-1]
+    parent_1 = course[-2]
+    parent_2 = course[-3] if len(course) > 2 else ""
+    s3.Object(BUCKET_NAME, f'data/{course_id}_data.csv').put(Body=f'{title}, {course_id}, {parent_1}, {parent_2}, {content}')
     
-    except:
-        print(f'error at course {course}')
 
 def main():
     targets = ['B', 'C', 'D']
