58e115f169504c6c75ba3c00cc0dbdc319ecef66
Manuel Lang <manuellang183@gmail.com>
2019-09-20 16:09:34 +0200

    backend information single course

--- backend/application.py
+++ backend/application.py
@@ -1,7 +1,8 @@
+from bson import json_util
+from bson.objectid import ObjectId
 from flask import Flask, render_template
 from flask_caching import Cache
 from flask_cors import CORS
-from bson import json_util
 import json
 import os
 from pymongo import MongoClient
@@ -40,23 +41,42 @@ def list_courses(limit):
 Lists all existing courses with a given title
 """
 @application.route("/courses/filter/<title>")
+@cache.cached(timeout=50)
 def find_all_courses(title):
     connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
-    courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': f'.*{title}.*' }}))
+    courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': re.compile(f'.*{title}.*', re.IGNORECASE) }}))
     return json.dumps(courses_collection, default=json_util.default)
 
 """
 Lists all existing courses with a given title and a specifc limit
 """
 @application.route("/courses/filter/<title>/<int:limit>")
+@cache.cached(timeout=50)
 def find_courses(title, limit):
     connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': f'.*{title}.*' }}).limit(limit))
     return json.dumps(courses_collection, default=json_util.default)
+
+
+@application.route("/courses/find/<id>")
+def find_course(id):
+    """Finds a specifc course that matches the given id
+    
+    Arguments:
+        id {String} -- course id in the database
+    
+    Returns:
+        JSON -- the matching course
+    """
+    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    mongo_client = MongoClient(connection_string)
+    t4g_database = mongo_client.t4g
+    course = t4g_database.courses.find_one({'_id': ObjectId(id)})
+    return json.dumps(course, default=json_util.default)
 
 
 if __name__ == "__main__":
