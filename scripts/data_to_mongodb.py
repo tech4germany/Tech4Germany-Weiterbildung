fba877c1797b7a4d45cedb2b5a1e236f1e7ee3d5
Manuel Lang <manuellang183@gmail.com>
2019-09-23 23:40:12 +0200

    updated data to db

--- scripts/data_to_mongodb.py
+++ scripts/data_to_mongodb.py
@@ -1,30 +1,29 @@
-import csv
 import json
 import os
 from pymongo import MongoClient
+import re
 from tqdm import tqdm as tqdm
 
 def import_files(collection, folder):
     files = os.listdir(folder)
-    csv_files = [input_file for input_file in files if input_file.endswith('_data.csv')]
-    for csv_file in tqdm(csv_files):
-        with open(os.path.join(folder, csv_file), 'r') as csvfile:
-            course_reader = csv.reader(csvfile, delimiter=',')
-            for row in course_reader:
-                if len(row) > 1:
-                    title = row[0].strip()
-                    course_id = row[1].strip()
-                    parent_1 = row[2].replace('+', ' ').strip()
-                    parent_2 = row[3].replace('+', ' ').strip()
-                    collection.insert_one({'title': title, 'parent_1': parent_1, 'parent_2': parent_2})
-                    break
+    data_files = [input_file for input_file in files if input_file.endswith('_data.txt')]
+    for data_file in tqdm(data_files):
+        with open(os.path.join(folder, data_file), 'r') as infile:
+            # load data
+            data = infile.read()
+            data = data.replace('\\n', ' ')
+            data = data.replace('\\t','')
+            data = json.loads(data)
+
+            # export data to database
+            collection.insert_one(data)
 
 def main():
     connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     courses_collection = t4g_database.courses
-    import_files(courses_collection, '../scraping/output/')
+    import_files(courses_collection, '../scraping/output_2/')
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
