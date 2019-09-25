f4b53218de06d0f7e4d9b7111ac8db512387f061
Florian Zechmeister <florian.zechmeister@nexos.de>
2019-09-25 17:36:24 +0200

    make things nice

--- frontend/src/pages/CardPage.js
+++ frontend/src/pages/CardPage.js
@@ -15,10 +15,20 @@ const CardPage = props =>  {
                     <Box><h1>Was gefällt dir besser?</h1></Box>
                 </Grid>
                 <Grid item xs={6}>
-                    <JobCard title={'Schweißen'} image_source={'./job_images/schweißen.jpg'} description={'Geprüfte Schweißer/innen verbinden Metallteile und montieren Anlagen- und Konstruktionsbauteile mittels unterschiedlicher Schweißverfahren.'} option_handler={props.option_handler}/>
+                    <JobCard 
+                        title={'Schweißen'} 
+                        image_source={'./job_images/schweißen.jpg'} 
+                        description={'Geprüfte Schweißer/innen verbinden Metallteile und montieren Anlagen- und Konstruktionsbauteile mittels unterschiedlicher Schweißverfahren.'} 
+                        option_handler={props.option_handler}
+                    />
                 </Grid>
                 <Grid item xs={6}>
-                    <JobCard title={'Töpfern'} image_source={'./job_images/töpfern.jpg'} description={'Keramiker/innen stellen Gebrauchskeramik, Baukeramik und Zierkeramik her. Ihre Aufgaben reichen von Planung und Entwurf über die Fertigung bis hin zum Verkauf.'} option_handler={props.option_handler}/>
+                    <JobCard 
+                        title={'Töpfern'} 
+                        image_source={'./job_images/töpfern.jpg'} 
+                        description={'Keramiker/innen stellen Gebrauchskeramik, Baukeramik und Zierkeramik her. Ihre Aufgaben reichen von Planung und Entwurf über die Fertigung bis hin zum Verkauf.'} 
+                        option_handler={props.option_handler}
+                    />
                 </Grid>
             </Grid>
     )
