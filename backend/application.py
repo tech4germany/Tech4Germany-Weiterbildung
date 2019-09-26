679c13ec956b5ae6e817ef5bc597e19c99b92626
Manuel Lang <manuellang183@gmail.com>
2019-09-26 15:03:43 +0200

    backend with .env and shell script to overcome flask bug

--- backend/application.py
+++ backend/application.py
@@ -1,10 +1,12 @@
 from bson import json_util
 from bson.objectid import ObjectId
+from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_caching import Cache
 from flask_cors import CORS
 import json
 import os
+from pathlib import Path
 from pymongo import MongoClient
 import re
 
@@ -19,7 +21,7 @@ Lists all existing courses
 @application.route("/courses/all")
 @cache.cached(timeout=50)
 def list_all_courses():
-    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    connection_string = f'mongodb+srv://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_URL")}'
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     courses_collection = list(t4g_database.courses.find({}))
@@ -31,7 +33,7 @@ Lists all existing courses
 @application.route("/courses/all/<int:limit>")
 @cache.cached(timeout=50)
 def list_courses(limit):
-    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    connection_string = f'mongodb+srv://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_URL")}'
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     courses_collection = list(t4g_database.courses.find({}).limit(limit))
@@ -43,7 +45,7 @@ Lists all existing courses with a given title
 @application.route("/courses/filter/<title>")
 @cache.cached(timeout=50)
 def find_all_courses(title):
-    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    connection_string = f'mongodb+srv://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_URL")}'
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': re.compile(f'.*{title}.*', re.IGNORECASE) }}))
@@ -55,7 +57,7 @@ Lists all existing courses with a given title and a specifc limit
 @application.route("/courses/filter/<title>/<int:limit>")
 @cache.cached(timeout=50)
 def find_courses(title, limit):
-    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    connection_string = f'mongodb+srv://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_URL")}'
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': f'.*{title}.*' }}).limit(limit))
@@ -72,7 +74,7 @@ def find_course(id):
     Returns:
         JSON -- the matching course
     """
-    connection_string = "mongodb+srv://t4g:bmas@cluster0-4lru4.mongodb.net/test"
+    connection_string = f'mongodb+srv://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_URL")}'
     mongo_client = MongoClient(connection_string)
     t4g_database = mongo_client.t4g
     course = t4g_database.courses.find_one({'_id': ObjectId(id)})
@@ -80,4 +82,6 @@ def find_course(id):
 
 
 if __name__ == "__main__":
-    application.run(debug=True, host="0.0.0.0")
+    env_path = os.path.join(os.path.dirname(__file__), '../.env')
+    load_dotenv(dotenv_path=env_path)
+    application.run(debug=True, host="0.0.0.0", port=os.getenv("BACKEND_PORT"))
