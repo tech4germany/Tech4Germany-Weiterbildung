ebc0ecf1ebabeaa5842ff9da62dcc177b1294204
Manuel Lang <manuellang183@gmail.com>
2019-09-26 17:41:50 +0200

    euclidean and cosine embeddings distance

--- scripts/process_embeddings.py
+++ scripts/process_embeddings.py
@@ -1,12 +1,13 @@
 import csv
 import numpy as np
+from scipy import spatial
 
 def embeddings_to_distance_matrix(embeddings):
     dist_matrix = []
     for i in range(len(embeddings)):
         distances = []
         for j in range(len(embeddings)):
-            distances.append(np.linalg.norm(np.array(embeddings[i])-np.array(embeddings[j])))
+            distances.append(spatial.distance.cosine(np.array(embeddings[i]), np.array(embeddings[j])))
         dist_matrix.append(distances)
     return dist_matrix
 
@@ -28,7 +29,7 @@ def main():
     embeddings_path = './embeddings.csv'
     embeddings = load_embeddings(embeddings_path)
     dist_matrix = embeddings_to_distance_matrix(embeddings)
-    export_dist_matrix(dist_matrix, "./embeddings_dists.csv")
+    export_dist_matrix(dist_matrix, "./embeddings_dists_cosine.csv")
 
 if __name__ == "__main__":
     main()
