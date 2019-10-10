531c356699b9e46bcee8431b3cb444a67f8f6fc9
Manuel Lang <manuellang183@gmail.com>
2019-10-10 16:26:28 +0200

    job features and backend update

--- backend/application.py
+++ backend/application.py
@@ -15,7 +15,7 @@ import uuid
 
 application = Flask(__name__)
 CORS(application)
-cache = Cache(application,config={'CACHE_TYPE': 'simple'})
+cache = Cache(application, config={'CACHE_TYPE': 'simple'})
 cache.init_app(application)
 
 """
@@ -34,7 +34,7 @@ Lists all existing courses
 @cache.cached(timeout=50)
 def list_courses(limit):
     courses_collection = list(t4g_database.courses.find({}).limit(limit))
-    return json.dumps(courses_collection, default=json_util.default)
+    return json.dumps(courses_collection, default=json_util.default), 200
 
 """
 Lists all existing courses with a given title
@@ -43,7 +43,7 @@ Lists all existing courses with a given title
 @cache.cached(timeout=50)
 def find_all_courses(title):
     courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': re.compile(f'.*{title}.*', re.IGNORECASE) }}))
-    return json.dumps(courses_collection, default=json_util.default)
+    return json.dumps(courses_collection, default=json_util.default), 200
 
 @application.route("/courses/filter/<title>/<int:limit>", methods=['GET'])
 @cache.cached(timeout=50)
@@ -58,7 +58,7 @@ def find_courses(title, limit):
         JSON -- a given amount of courses matching the given title
     """
     courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': f'.*{title}.*' }}).limit(limit))
-    return json.dumps(courses_collection, default=json_util.default)
+    return json.dumps(courses_collection, default=json_util.default), 200
 
 @application.route("/courses/find/<id>", methods=['GET'])
 def find_course(id):
@@ -71,29 +71,46 @@ def find_course(id):
         JSON -- the matching course
     """
     course = t4g_database.courses.find_one({'_id': ObjectId(id)})
-    return json.dumps(course, default=json_util.default)
+    return json.dumps(course, default=json_util.default), 200
 
 @application.route("/select", methods=['POST'])
 def set_option():
-    _uuid = request.get_json('uuid')['uuid']
-    option = request.get_json('option')['option']
-    session = t4g_database.sessions.find_one({'uuid': uuid.UUID(_uuid).hex})
-    session['selected'].append(option)
-    session['options'].remove(option)
-    for val in session['options']:
-        session['options'].remove(val)
-        session['not_selected'].append(val)
-    session['options'] = _get_options(session['selected'], session['not_selected'])
-    t4g_database.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
-    return json.dumps(session, default=json_util.default)
+    if request.get_json('option_type')['option_type'] == "Berufe":
+        # store selected job option and send the session information with generated options
+        _uuid = request.get_json('uuid')['uuid']
+        option = request.get_json('option')['option']
+        session = t4g_database.sessions.find_one({'uuid': uuid.UUID(_uuid).hex})
+        session['selected'].append(option)
+        session['options'].remove(option)
+        for val in session['options']:
+            session['options'].remove(val)
+            session['not_selected'].append(val)
+        session['options'] = _get_options(session['selected'], session['not_selected'])
+        t4g_database.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
+
+        # also send results
+        return json.dumps(session, default=json_util.default)
+    elif request.get_json('option_type')['option_type'] == "Kategorien":
+        # store selected categories and send the session with initial options
+        _uuid = request.get_json('uuid')['uuid']
+        categories = request.get_json('categories')['categories']
+        start_jobs_titles = []
+        for category in categories:
+            job_id = t4g_database.categories.find_one({"category_name": category})['job_id']
+            related_job = _load_related_job(job_id)
+            start_jobs_titles.append(related_job['title'])
+        job_embeddings = [_get_job_embedding(x) for x in start_jobs_titles]
+        options = _load_init_options(job_embeddings)
+        resp_data = {}
+        return resp_data
+    else:
+        return "Invalid option type, please use either 'Berufe' or 'Kategorien'", 406
 
 @application.route("/init", methods=['GET'])
 def init_session():
     resp = {}
     resp['uuid'] = uuid.uuid4().hex
-    resp['options'] = _find_seed_values(cosine_distances, average_distance, entities)
-    resp['selected'] = []
-    resp['not_selected'] = []
+    resp['categories'] = _load_categories()
     t4g_database.sessions.insert_one(resp)
     return json.dumps(resp, default=json_util.default)
 
@@ -114,59 +131,126 @@ def _get_options(selected, not_selected, neighborhood_size = 10, num_pts = 2):
     neighbors = list(np.random.choice(rel_neighbors, num_pts, replace=False))
     return [entities[x] for x in neighbors]
 
-def _load_entities():
-    entities = []
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/embeddings.csv'), 'r') as embeddings_in:
-        data = csv.reader(embeddings_in, delimiter=',')
-        for line in data:
-            entities.append(line[0])
-    return entities
+def _load_init_options(job_titles):
+    if len(job_titles) == 1:
+        # suggest two jobs within a rather close distance to the given embedding
+        options = _find_close_point(job_titles[0], n_pts=2)
+    else:
+        if len(job_embeddings) > 2:
+            # select two random samples
+            job_embeddings = np.random.choice(job_titles, 2, replace=False)
 
-def _load_embeddings():
-    embeddings = []
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/embeddings.csv'), 'r') as infile:
+        # suggest one job within a rather close distance each for both of the given embeddings
+        for job_title in job_titles:
+            option = _find_close_point(job_title)
+
+def _find_close_point(job_titles, n_pts=1):
+    dists = []
+    for job_title in job_titles:
+        i = entities.index(job_title)
+        dists = dist_matrix[i]
+        neighbors = np.argpartition(dists, 30)[1:30]
+        print(i, neighbors)
+
+# def _load_entities():
+#     entities = []
+#     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/embeddings.csv'), 'r') as embeddings_in:
+#         data = csv.reader(embeddings_in, delimiter=',')
+#         for line in data:
+#             entities.append(line[0])
+#     return entities
+
+# def _load_embeddings():
+#     embeddings = []
+#     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/embeddings.csv'), 'r') as infile:
+#         data = csv.reader(infile, delimiter=',')
+#         for line in data:
+#             embedding = [float(x) for x in line[1:]]
+#             embeddings.append(embedding)
+#     return embeddings
+
+# def _load_cosine_distances():
+#     cosine_distances = []
+#     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/embeddings_dists_cosine.csv'), 'r') as dists_in:
+#         data = csv.reader(dists_in, delimiter=',')
+#         for line in data:
+#             cosine_distances.append([float(x) for x in line])
+#     print("Done")
+#     return cosine_distances
+
+# def _get_unique_distances(distances):
+#     """Gets all unique distances from a n x n distance matrix where i != j
+    
+#     Arguments:
+#         distances {list<list<float>>} -- an n x n distance matrix describing all pairwise distances between points i and j
+    
+#     Returns:
+#         list<float> -- all unique distance with i != j
+#     """
+#     unique_distances = []
+#     for i in range(len(distances)):
+#         for j in range(i+1,len(distances)):
+#             unique_distances.append(distances[i][j])
+#     return unique_distances
+
+# def _get_average_cosine_distance(unique_distances):
+#     return sum(unique_distances) / len(unique_distances)
+
+# def _find_seed_points(cosine_distance_matrix, average_distance, num_pts):
+#     seed_pts = np.random.choice(np.arange(len(cosine_distance_matrix[0])), num_pts)
+#     for index in range(len(seed_pts)-1):
+#         if cosine_distance_matrix[seed_pts[index]][seed_pts[index+1]] < average_distance:
+#             return _find_seed_points(cosine_distance_matrix, average_distance, num_pts)
+#     return seed_pts
+
+# def _find_seed_values(cosine_distance_matrix, average_distance, entities, num_pts = 2):
+#     seed_pts = _find_seed_points(cosine_distance_matrix, average_distance, num_pts)
+#     return [entities[x] for x in seed_pts]
+
+def _load_categories():
+    categories = list(t4g_database.categories.find({}))
+    return json.dumps(categories, default=json_util.default)
+
+def _load_related_job(job_id):
+    return t4g_database.jobs.find_one({'job_id': job_id})
+
+def _load_jobs_data(file_name):
+    embeddings = dict()
+    entities = []
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name), 'r') as infile:
         data = csv.reader(infile, delimiter=',')
         for line in data:
-            embedding = [float(x) for x in line[1:]]
-            embeddings.append(embedding)
-    return embeddings
+            embeddings[line[0]] = [float(x) for x in line[1:]]
+            entities.append(line[0])
+    return entities, embeddings
 
-def _load_cosine_distances():
-    cosine_distances = []
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../data/embeddings_dists_cosine.csv'), 'r') as dists_in:
-        data = csv.reader(dists_in, delimiter=',')
-        for line in data:
-            cosine_distances.append([float(x) for x in line])
-    return cosine_distances
+# def _load_job_entities(file_name):
+#     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), file_name), 'r') as infile:
+#         data = csv.reader(infile, delimiter=',')
+#         entities = [line[0] for line in data]
+#     return entities
 
-def _get_unique_distances(distances):
-    """Gets all unique distances from a n x n distance matrix where i != j
-    
-    Arguments:
-        distances {list<list<float>>} -- an n x n distance matrix describing all pairwise distances between points i and j
-    
-    Returns:
-        list<float> -- all unique distance with i != j
-    """
-    unique_distances = []
-    for i in range(len(distances)):
-        for j in range(i+1,len(distances)):
-            unique_distances.append(distances[i][j])
-    return unique_distances
+def _get_job_embedding(job):
+    return job_embeddings[job]
 
-def _get_average_cosine_distance(unique_distances):
-    return sum(unique_distances) / len(unique_distances)
+# def _load_dists(job_embeddings):
+#     print("Load jobs embeddings.")
+#     dist_matrix = []
+#     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../data/jobs_cosine_distances.csv"), 'a+') as outfile:
+#         for key_i in job_embeddings:
+#             dists = []
+#             for key_j in job_embeddings:
+#                 dists.append(spatial.distance.cosine(job_embeddings[key_i], job_embeddings[key_j]))
+#             outfile.write(f'{",".join([str(x) for x in dists])}\n')
+#             dist_matrix.append(dists)
+#     print("Done.")
+#     return dist_matrix
 
-def _find_seed_points(cosine_distance_matrix, average_distance, num_pts):
-    seed_pts = np.random.choice(np.arange(len(cosine_distance_matrix[0])), num_pts)
-    for index in range(len(seed_pts)-1):
-        if cosine_distance_matrix[seed_pts[index]][seed_pts[index+1]] < average_distance:
-            return _find_seed_points(cosine_distance_matrix, average_distance, num_pts)
-    return seed_pts
-
-def _find_seed_values(cosine_distance_matrix, average_distance, entities, num_pts = 2):
-    seed_pts = _find_seed_points(cosine_distance_matrix, average_distance, num_pts)
-    return [entities[x] for x in seed_pts]
+def _load_dists(file_path):
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), file_path), r) as in_file:
+        data = csv.reader(infile, delimiter=',')
+        dist_matrix [float(y) for x in data for y in x]
+    return dist_matrix
 
 if __name__ == "__main__":
     # load environment variables
@@ -179,11 +263,8 @@ if __name__ == "__main__":
     t4g_database = mongo_client.test
 
     # load embeddings and entities
-    entities = _load_entities()
-    embeddings = _load_embeddings()
-    cosine_distances = _load_cosine_distances()
-    unique_distances = _get_unique_distances(cosine_distances)
-    average_distance = _get_average_cosine_distance(unique_distances)
+    job_entities, job_embeddings = _load_jobs_data('../data/job_embeddings.csv')
+    dist_matrix = _load_dists(job_embeddings)
 
     # start application
     application.run(debug=True, host="0.0.0.0", port=os.getenv("BACKEND_PORT"))
---
+++ scripts/get_jobs_2d.py
@@ -0,0 +1,41 @@
+from sklearn.manifold import TSNE
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+def tsne_features(jobs):
+    print('========== Fitting T-SNE ==========')
+    features = []
+    for key in jobs:
+        features.append(jobs[key])
+    tsne_ak_2d = TSNE(perplexity=30, n_components=2, init='pca', n_iter=3500, random_state=32)
+    jobs_ak_2d = tsne_ak_2d.fit_transform(features)
+    plt.figure(figsize=(16, 9))
+    x = jobs_ak_2d[:,0]
+    y = jobs_ak_2d[:,1]
+    plt.scatter(x, y, alpha = 0.05)
+    for i, key in enumerate(jobs):
+        interest_points = ['abfallbeauftragte/r''afrikanist/in', 'altenpfleger/in', 'anlageberater/in', 'apotheker/in', 'architekt/in', 'arzt/ärztin', 'betriebswirt/in (bbig)', 'bildeinrahmer/in', 'blockchain-entwickler/in', 'bodensteward/-stewardess', 'dachdecker/in', 'detektiv/in', 'fachapotheker/in', 'fahrzeugpfleger/in', 'fitnesstrainer/in', 'fluglehrer/in' 'forstmaschinenführer/in', 'fußballtrainer/in', 'gamedesigner/in', 'geofraf/in', 'gestalter/in - produktdesign', 'gewandmeister/in', 'imam', 'inkassobeauftragte/r (außendienst)', 'jurist/in', 'klauenpfleger/in' 'modist/in', 'politiker/in', 'regisseur/in', 'rettungshelfer/in', 'schweißer/in', 'schiffssteward/-stewardess', 'softwareentwickler/in', 'sportgerätebauer/in', 'verkaufstrainer/in', 'wahrsager/in', 'wett- und lotterieannehmer/in']
+        if key.lower() in interest_points:
+            plt.annotate(key, xy=(x[i], y[i]), xytext=(5, 2), 
+                        textcoords='offset points', ha='right', va='bottom', size=8)
+        # plt.annotate(key, xy=(x[i], y[i]), xytext=(5, 2), color='blue',
+        #             textcoords='offset points', ha='right', va='bottom', size=8)
+    plt.legend(loc=4)
+    plt.xlabel = "T-distributed Stochastic Neighbor Embedding Dimension 1"
+    plt.ylabel = "T-distributed Stochastic Neighbor Embedding Dimension 2"
+    plt.title('Repräsentationen von Berufstiteln anhand von Embeddings der Tätigkeitsbeschreibungen')
+    plt.show()
+    print('========== Done Fitting T-SNE ==========')
+
+def main():
+    jobs = dict()
+    line_nr = 0
+    with open("./job_features.csv", 'r') as infile:
+        for line in infile:
+            data = line.strip().split(',')
+            jobs[data[0]] = data[1:]
+            line_nr += 1
+    tsne_features(jobs)
+
+if __name__ == "__main__":
+    main()
