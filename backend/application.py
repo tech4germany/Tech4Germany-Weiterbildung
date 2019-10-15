3324d7a54a2f5d29baddc3ba7fa175036fe55203
Manuel Lang <manuellang183@gmail.com>
2019-10-15 10:35:15 +0200

    backend documentation and runtime improvements

--- backend/application.py
+++ backend/application.py
@@ -24,16 +24,21 @@ Lists all existing courses
 @application.route("/courses/all", methods=['GET'])
 @cache.cached(timeout=50)
 def list_all_courses():
-    courses_collection = list(t4g_database.courses.find({}))
+    courses_collection = list(mongo_client.test.courses.find({}))
     return json.dumps(courses_collection, default=json_util.default)
 
-"""
-Lists all existing courses
-"""
 @application.route("/courses/all/<int:limit>", methods=['GET'])
 @cache.cached(timeout=50)
 def list_courses(limit):
-    courses_collection = list(t4g_database.courses.find({}).limit(limit))
+    """Lists all existing courses with a given limit
+    
+    Arguments:
+        limit {int} -- amount of shown courses
+    
+    Returns:
+        JSON -- the first {limit} courses
+    """
+    courses_collection = list(mongo_client.test.courses.find({}).limit(limit))
     return json.dumps(courses_collection, default=json_util.default)
 
 @application.route("/courses/filter/<title>", methods=['GET'])
@@ -45,9 +50,9 @@ def find_all_courses_with_title(title):
         title {String} -- the search title
     
     Returns:
-        [type] -- [description]
+        JSON -- courses matching a given title
     """
-    courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': re.compile(f'.*{title}.*', re.IGNORECASE) }}))
+    courses_collection = list(mongo_client.test.courses.find({ 'title': { '$regex': re.compile(f'.*{title}.*', re.IGNORECASE) }}))
     return json.dumps(courses_collection, default=json_util.default)
 
 @application.route("/courses/filter/<title>/<int:limit>", methods=['GET'])
@@ -62,7 +67,7 @@ def find_courses(title, limit):
     Returns:
         JSON -- a given amount of courses matching the given title
     """
-    courses_collection = list(t4g_database.courses.find({ 'title': { '$regex': f'.*{title}.*' }}).limit(limit))
+    courses_collection = list(mongo_client.test.courses.find({ 'title': { '$regex': f'.*{title}.*' }}).limit(limit))
     return json.dumps(courses_collection, default=json_util.default)
 
 @application.route("/courses/find/<id>", methods=['GET'])
@@ -75,18 +80,18 @@ def find_course(id):
     Returns:
         JSON -- the matching course
     """
-    course = t4g_database.courses.find_one({'_id': ObjectId(id)})
+    course = mongo_client.test.courses.find_one({'_id': ObjectId(id)})
     return json.dumps(course, default=json_util.default)
 
 @application.route("/select", methods=['POST'])
 def set_option():
-    """[summary]
+    """Select an option (either category or job) and generate two new job options
     
     Returns:
-        [type] -- [description]
+        JSON -- a session including two generated options
     """
     _uuid = request.get_json('uuid')['uuid']
-    session = t4g_database.sessions.find_one({'uuid': uuid.UUID(_uuid).hex})
+    session = mongo_client.test.sessions.find_one({'uuid': uuid.UUID(_uuid).hex})
     if request.get_json('option_type')['option_type'] == "Berufe":
         # store selected job option and send the session information with generated options
         option = request.get_json('options')['options'][0]
@@ -102,21 +107,21 @@ def set_option():
         for option in options:
             option_object = {}
             option_object['title'] = option
-            option_object['info'] = utils.get_job_info(t4g_database, option)
+            option_object['info'] = utils.get_job_info(mongo_client.test, option)
             option_objects.append(option_object)
         session['options'] = option_objects
         session['final'] =  0 if len(session['options']) > 0 else 1
 
-        t4g_database.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
-        return 200 if not application.debug else json.dumps(session, default=json_util.default)
+        mongo_client.test.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
+        return json.dumps(session, default=json_util.default)
 
     elif request.get_json('option_type')['option_type'] == "Branchen":
         # store selected categories and send the session with initial options
         categories = request.get_json('options')['options']
         start_jobs_titles = []
         for category in categories:
-            job_id = t4g_database.categories.find_one({"category_name": category['title']})['job_id']
-            related_job = utils.load_related_job(t4g_database ,job_id)
+            job_id = mongo_client.test.categories.find_one({"category_name": category['title']})['job_id']
+            related_job = utils.load_related_job(mongo_client.test ,job_id)
             start_jobs_titles.append(related_job['title'])
 
         options = utils.load_init_options(dist_matrix, job_entities, start_jobs_titles)
@@ -124,53 +129,58 @@ def set_option():
         for option in options:
             option_object = {}
             option_object['title'] = option
-            option_object['info'] = utils.get_job_info(t4g_database, option)
+            option_object['info'] = utils.get_job_info(mongo_client.test, option)
             option_objects.append(option_object)
         session['options'] = option_objects
         session['option_type'] = "Berufe"
-        t4g_database.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
-        return 200 if not application.debug else json.dumps(session, default=json_util.default)
+        mongo_client.test.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
+        return json.dumps(session, default=json_util.default)
     else:
         return "Invalid option type, please use either 'Berufe' or 'Branchen'", 406
 
 @application.route("/init", methods=['GET'])
 def init_session():
-    """[summary]
+    """Initialize a session
     
     Returns:
-        [type] -- [description]
+        JSON -- a generate session containing a UUID and the different categories
     """
     session = {}
     session['uuid'] = uuid.uuid4().hex
-    options = utils.load_categories(t4g_database)
+    options = utils.load_categories(mongo_client.test)
     option_objects = []
     for option in options:
             option_object = {}
             option_object['title'] = option
-            option_object['info'] = utils.get_category_info(t4g_database, option)
+            option_object['info'] = utils.get_category_info(mongo_client.test, option)
             option_objects.append(option_object)
     session['options'] = option_objects
     session['option_type'] = "Branchen"
     session['fav_jobs'] = session['fav_courses'] = session['selected'] = session['not_selected'] = []
-    t4g_database.sessions.insert_one(session)
+    mongo_client.test.sessions.insert_one(session)
     return json.dumps(session, default=json_util.default)
 
 @application.route("/like", methods=['POST'])
 def like_item():
-    """[summary]
+    """Like an item (job or course)
     
     Returns:
-        [type] -- [description]
+        int -- status code
     """
     _uuid = request.get_json('uuid')['uuid']
     title = request.get_json('options')['options']
-    session = t4g_database.sessions.find_one({"uuid": _uuid})
+    session = mongo_client.test.sessions.find_one({"uuid": _uuid})
+
+    # add liked course
     if request.get_json('option_type')['option_type'] == "Kurse":
         session['fav_courses'].append(title)
+    
+    # add liked job
     elif request.get_json('option_type')['option_type'] == "Berufe":
         session['fav_jobs'].append(title['title'])
 
-    t4g_database.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
+    # update session
+    mongo_client.test.sessions.update_one({'uuid': uuid.UUID(_uuid).hex}, {'$set': session})
     return 200 if not application.debug else json.dumps(session, default=json_util.default)
 
 @application.route("/courses/add", methods=['POST'])
@@ -182,9 +192,10 @@ def add_course():
     """
     course = {}
     course['title'] = request.get_json('title')['title']
+    course['info'] = request.get_json('info')['info']
     # TODO add all required fields
 
-    result = t4g_database.courses.insert_one(course)
+    result = mongo_client.test.courses.insert_one(course)
     return 200 if not application.debug else result
 
 @application.route("/courses/delete", methods=['POST'])
@@ -196,7 +207,7 @@ def delete_course():
     """
     # NOTE that deleting only according to the title might not be save
     # TODO use multiple identifers
-    result = t4g_database.courses.delete_one({"title": request.get_json('title')['title']})
+    result = mongo_client.test.courses.delete_one({"title": request.get_json('title')['title']})
     return 200 if not application.debug else result
 
 if __name__ == "__main__":
@@ -207,11 +218,11 @@ if __name__ == "__main__":
     # connect to database
     connection_string = os.getenv("DATABASE_URL")
     mongo_client = MongoClient(connection_string)
-    t4g_database = mongo_client.test
 
     # load embeddings and entities
     job_entities, job_embeddings = utils.load_jobs_data('../data/job_embeddings.csv')
     dist_matrix = utils.load_dists('../data/jobs_cosine_distances.csv')
 
     # start application
+    # NOTE not to use debug mode in production
     application.run(debug=True, host="0.0.0.0", port=os.getenv("BACKEND_PORT"))
